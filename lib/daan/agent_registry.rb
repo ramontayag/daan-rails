a7437736b330a72a77038aa2b219e8e63384810e
Ramon Tayag <ramon.tayag@gmail.com>
2026-03-06 16:48:53 +0100

    feat: Daan::Agent plain Ruby struct and AgentRegistry

---
+++ lib/daan/agent_registry.rb
@@ -0,0 +1,24 @@
+# lib/daan/agent_registry.rb
+module Daan
+  class AgentRegistry
+    @registry = {}
+
+    class << self
+      def register(agent)
+        @registry[agent.name] = agent
+      end
+
+      def find(name)
+        @registry.fetch(name) { raise KeyError, "No agent registered: #{name.inspect}" }
+      end
+
+      def all
+        @registry.values
+      end
+
+      def clear
+        @registry = {}
+      end
+    end
+  end
+end
